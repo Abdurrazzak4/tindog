7f7ab597f57ad3d89ea315249daef7ec942e38fd
Abdurrazzak Saymaz <abdurrazzak1905@gmail.com>
2023-07-27 16:15:58 +0300

    upd

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Remember to import the data and Dog class!
+
+import { dogs } from "./data.js"
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getDogHtml(){
+document.getElementById("body").innerHTML = `
+<section style="background-image: url(${avatar})">
+      <div>${name}, <span>${age}</span></div>
+      <div id="bio">${bio}</div>
+    </section>
+    <div class="bttm-icons">
+      <img src="images/icon-cross.png" alt="icon cross" />
+      <img src="images/icon-heart.png" alt="icon heart" />
+    </div>
+`
+}
+
+getDogHtml()
