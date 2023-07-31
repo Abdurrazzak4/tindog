810cd04dfb6112b4461ac45e8256877ddddbb197
Abdurrazzak Saymaz <abdurrazzak1905@gmail.com>
2023-07-31 18:27:20 +0300

    upd

--- Dog.js
+++ Dog.js
@@ -1,4 +1,46 @@
 // Create the Dog class here
+import dogs from "./data.js"
+
+class Dog {
+
+    constructor(data){
+       Object.assign(this,data)
+    }
+
+    getDogData(){
+        return this;
+    }
+
+    getDogHtml(){
+       let {name,avatar,age,bio,hasBeenSwiped,hasBeenLiked} = this
+
+        return `
+                        <img class="dog-image" src="${avatar}" id=${name} />
+                        <div class="pdiv" ><p class="p1">${name}, ${age}</p>
+                        <p class="p2">${bio}</p></div>
+
+                 `
+    }
+
+    getLikeBadgeHtml(){
+        return `
+                                <div class="rec">
+                                <img src="images/badge-like.png" class="like" id="like" />  
+                                </div>              
+                            `
+    }
+
+    getNopeBadgeHtml(){
+        return `<div class="rec">
+                <img src="images/badge-nope.png" class="dislike" id="dislike" />  
+                </div>                                                                 
+                `
+    }
 
 
 
+
+
+}
+
+export default Dog
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
-// Remember to import the data and Dog class!
-
-import { dogsData } from "./data.js";
+import {addBadge, swipeVertically, getNewDog } from "./utils.js"
 
 
-function renderDogs(){
-    getDogHtml(dogsData.Rex)
+
+const acceptBtn = document.getElementById('accept')
+const rejectBtn = document.getElementById('reject')
+
+
+function render() {
+    let dog = getNewDog()
+    document.getElementById('dog-desc').innerHTML = dog.getDogHtml()
+    addBadge(dog)
 }
 
+render()
 
 
 
 
-function getDogHtml(data) {
-document.getElementById("body").innerHTML = `
-    <section style="background-image: url(${data.avatar})">
-  <div>${data.name}, <span>${data.age}</span></div>
-  <div id="bio">${data.bio}</div>
-</section>
-<div class="bttm-icons">
-  <img id="cross-btn" src="images/icon-cross.png" alt="icon cross" />
-  <img id="hearth-btn" src="images/icon-heart.png" alt="icon heart" />
-</div>
-    `;
-}
 
-renderDogs()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
